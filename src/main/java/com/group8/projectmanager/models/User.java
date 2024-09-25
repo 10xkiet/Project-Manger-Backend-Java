540b2b444d4c183043b5f4dfb7212038911f01ad
minhnbnt <minhnbnt@gmail.com>
2024-09-25 21:41:57 +0700

    feat: add some model constraints

--- src/main/java/com/group8/projectmanager/models/User.java
+++ src/main/java/com/group8/projectmanager/models/User.java
@@ -1,9 +1,6 @@
 package com.group8.projectmanager.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,7 +20,10 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     @Override
