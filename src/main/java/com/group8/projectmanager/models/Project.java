906e3819059cbdd345a8a972bbe11e3a0fa73382
10xkiet <thao655thao655@gmail.com>
2024-10-15 20:29:24 +0700

    Update Project.java

--- src/main/java/com/group8/projectmanager/models/Project.java
+++ src/main/java/com/group8/projectmanager/models/Project.java
@@ -13,27 +13,36 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Project  {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     @Column(nullable = false)
     private String projectName;
+    
     @Column(nullable = false)
     private LocalDateTime creationDate;
+    
     @Column()
     private LocalDateTime endDate;
+    
     @JoinColumn(name="creator_id",referencedColumnName = "id")
     @ManyToOne
     private User creator;
+    
     @Column(name="sub_projects")
     @OneToMany(mappedBy = "id",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Project> subProjects = new ArrayList<>() ;
+    
     @ManyToOne
     @JoinColumn(name = "parent_project_id",referencedColumnName = "id")
     private Project parentProject;
+    
     @ManyToOne
     @JoinColumn(name="manager_id",referencedColumnName = "id")
     private User manager;
+    
     @PrePersist
     protected void onCreate(){
         this.creationDate=LocalDateTime.now();
