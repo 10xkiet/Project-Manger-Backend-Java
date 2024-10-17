cf9ad6b4543f77f12e03776ed628b3513dad7e87
minhnbnt <minhnbnt@gmail.com>
2024-10-17 13:57:53 +0700

    fix: fixed missing repository decorator in InvitationRepository

--- src/main/java/com/group8/projectmanager/repositories/InvitationRepository.java
+++ src/main/java/com/group8/projectmanager/repositories/InvitationRepository.java
@@ -2,9 +2,11 @@ package com.group8.projectmanager.repositories;
 
 import com.group8.projectmanager.models.Invitation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.stream.Stream;
 
+@Repository
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
     Stream<Invitation> findByReceiverId(Long id);
 }
