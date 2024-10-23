cbad7b97120377fa0fd189fcac791bd9b0096e6d
minhnbnt <minhnbnt@gmail.com>
2024-10-23 18:46:35 +0700

    fix: added missing Cors config

---
+++ src/main/java/com/group8/projectmanager/configs/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.group8.projectmanager.configs;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*");
+    }
+}
