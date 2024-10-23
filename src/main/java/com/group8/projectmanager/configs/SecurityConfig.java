a5934e827e9557abe7a68a5f558911e338c57bf0
minhnbnt <minhnbnt@gmail.com>
2024-10-23 19:06:09 +0700

    fix: switched to global Cors config

--- src/main/java/com/group8/projectmanager/configs/SecurityConfig.java
+++ src/main/java/com/group8/projectmanager/configs/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +33,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
+
+            .cors(cors ->
+                cors.configurationSource(req ->
+                    new CorsConfiguration().applyPermitDefaultValues()
+                )
+            )
+
             .csrf(CsrfConfigurer::disable)
 
             .authorizeHttpRequests(authorize -> {
